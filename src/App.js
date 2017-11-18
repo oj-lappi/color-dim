d0f7563499c362a92b165676942db3aba7f6b092
Oskar Lappi <olappi@abo.fi>
2017-11-18 20:51:25 +0000

    init commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import BarChart from './BarChart'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+        <h1 className="Viz-title">Plot</h1>
+        </header>
+	    <div>
+	<BarChart data={[5,12,10,4.4,10,5.6,3,2]} size={[600,600]} />
+	    </div>
+      </div>
+    );
+  }
+}
+
+export default App;
