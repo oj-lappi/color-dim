1ceae0e9ebd24d8fd525bb71a31096de7470679b
oj <oj.lappi@gmail.com>
2017-11-24 01:09:12 +0200

    openGL tests

--- src/Globe.js
+++ src/Globe.js
@@ -19,13 +19,77 @@ class Globe extends Component {
 	}
 
 	createViz(){
+		const canvas = document.querySelector("#glGlobeCanvas");
+		const gl = canvas.getContext("webgl");
+		if(!gl){
+			return;	
+		}
+		
+		gl.clearColor(0.0,0.0,0.0,1.0);
+		gl.clear(gl.COLOR_BUFFER_BIT);
+		const buffers = this.initBuffers(gl);
+		const shaders = this.initShaders(gl);
 
+		
+		//Bind vertex position buffer
+		gl.bindBuffer(gl.ARRAY_BUFFER, buffers["vposb"]);
+		
+		this.drawTriangle(gl);
    	}
 
+	initShaders(gl){
+		const vsSource = `
+			attribute vec4 aVertexPosition;
+			
+			uniform mat4 uModelViewMatrix;
+			uniform mat4 uProjectionMatrix;
+
+			void main(){
+				gl_position = uProjectionMatrix * uModelViewMatrix* avertexPosition;
+			}
+		`;
+
+		return {
+			"vertex":vsSource
+		};
+	}
+
+
+	initBuffers(gl){
+		const vertexPositionBuffer = gl.createBuffer();
+		
+
+		return {
+			"vposb":vertexPositionBuffer
+
+		};
+	}
+	drawTriangle(gl){
+		const vertices =
+		[
+			-0.90	,-0.90,  0.0,
+			 0.85	,-0.90,  0.0,
+			-0.90	, 0.85,  0.0,
+		
+			 0.90	,-0.85,  0.0,
+			 0.90	, 0.90,  0.0,
+			-0.85	, 0.90,  0.0
+		];
+		
+		gl.bufferData(gl.ARRAY_BUFFER,new Float32Array(vertices),gl.STATIC_DRAW);
+		//this.drawScene();
+	}	
+	drawScene(gl){
+		gl.viewport(0,0,gl.viewportWidth, gl.viewportHeight);
+		gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+	}
 	render() {
       		return 	<canvas
-			
+			id ="glGlobeCanvas"	
+			width="500" height="500"
 			> 
+
       			</canvas>
    	}
 }
